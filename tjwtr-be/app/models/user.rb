753846d71eb784898b3f1e917298a0ec5e9ce9ea
Dmitry Pikulkin <dmitry.pikulkin@toptal.com>
2017-11-13 18:13:14 +0200

    fix migration, added jwt gem

--- tjwtr-be/app/models/user.rb
+++ tjwtr-be/app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :posts
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
--- tjwtr-be/db/migrate/20171113160355_create_posts.rb
+++ tjwtr-be/db/migrate/20171113160355_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.0]
   def change
     create_table :posts do |t|
-      t.int :user_id
+      t.integer :user_id
       t.string :content
 
       t.timestamps
